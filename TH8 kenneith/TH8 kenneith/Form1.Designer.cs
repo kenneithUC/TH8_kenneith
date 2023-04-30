4c37388af93186b0e92659858bbfc142881dc611
kenneithUC <127652072+kenneithUC@users.noreply.github.com>
2023-05-01 01:06:08 +0700

    Add files via upload

---
+++ TH8 kenneith/TH8 kenneith/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TH8_kenneith
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.playerDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findMatchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playerDataToolStripMenuItem,
+            this.findMatchToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1328, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // playerDataToolStripMenuItem
+            // 
+            this.playerDataToolStripMenuItem.Name = "playerDataToolStripMenuItem";
+            this.playerDataToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.playerDataToolStripMenuItem.Text = "player data";
+            this.playerDataToolStripMenuItem.Click += new System.EventHandler(this.playerDataToolStripMenuItem_Click);
+            // 
+            // findMatchToolStripMenuItem
+            // 
+            this.findMatchToolStripMenuItem.Name = "findMatchToolStripMenuItem";
+            this.findMatchToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.findMatchToolStripMenuItem.Text = "find match";
+            this.findMatchToolStripMenuItem.Click += new System.EventHandler(this.findMatchToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(642, 683);
+            this.panel1.TabIndex = 2;
+            this.panel1.Visible = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(412, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(222, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectionChangeCommitted += new System.EventHandler(this.comboBox2_SelectionChangeCommitted);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(186, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(221, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(642, 25);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(686, 686);
+            this.panel2.TabIndex = 4;
+            this.panel2.Visible = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dataGridView3);
+            this.panel4.Controls.Add(this.dataGridView2);
+            this.panel4.Controls.Add(this.dataGridView1);
+            this.panel4.Location = new System.Drawing.Point(0, 163);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1325, 548);
+            this.panel4.TabIndex = 7;
+            this.panel4.Visible = false;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(681, 57);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(563, 231);
+            this.dataGridView3.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(31, 57);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(550, 231);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(352, 323);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(560, 204);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(255, 69);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(726, 21);
+            this.comboBox4.TabIndex = 1;
+            this.comboBox4.SelectionChangeCommitted += new System.EventHandler(this.comboBox4_SelectionChangeCommitted);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(500, 25);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(215, 21);
+            this.comboBox3.TabIndex = 0;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            this.comboBox3.SelectionChangeCommitted += new System.EventHandler(this.comboBox3_SelectionChangeCommitted);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.comboBox3);
+            this.panel3.Controls.Add(this.comboBox4);
+            this.panel3.Location = new System.Drawing.Point(1, 25);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1324, 141);
+            this.panel3.TabIndex = 9;
+            this.panel3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1328, 711);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem playerDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findMatchToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ TH8 kenneith/TH8 kenneith/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TH8_kenneith
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connection = "server=localhost;uid=root;pwd=;database=premier_league";
+        MySqlConnection sqlConnection;
+        MySqlCommand mysqlcommand;
+        MySqlDataAdapter MySqlDataAdapter;
+        string team="";
+        string player = "";
+        string playerprofile = "";
+        string playerprofile2 = "";
+        string teammatch = "";
+        string vsmatch="";
+        string matchdetail = "";
+        string teamhome = "";
+        string teamaway = "";
+        Label[] label1;
+        Label[] label3; 
+        Label[] label4;
+        DataTable dtteam=new DataTable();
+        DataTable dtplayer=new DataTable();
+        DataTable dtplayerprofile=new DataTable();
+        DataTable dtplayerprofile2=new DataTable();
+        DataTable dtteammatch = new DataTable();
+        DataTable dtvsmatch = new DataTable();
+        DataTable dtmatchdetail = new DataTable();
+        DataTable dtteamhome = new DataTable();
+        DataTable dtteamaway = new DataTable();
+
+
+        string label= "Player Name:,Team Name:,Nationality:,Position:,Yellow Card:,Red Card:,Goal Scored:,Own Goal:,Goal Penalty:,Penalty Miss:";
+        private void playerDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dtplayer.Clear();
+            dtteam.Clear();
+            panel3.Visible = false;
+            panel4.Visible = false;
+            panel1.Visible = true;
+            panel2.Visible = true;
+            team = "select team_id as 'ID',team_name as'Team Name' from team";
+            sqlConnection = new MySqlConnection(connection);
+            mysqlcommand = new MySqlCommand(team,sqlConnection);
+            MySqlDataAdapter=new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtteam);
+            comboBox1.DataSource = dtteam;
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "Team Name";
+            comboBox1.Text = "";
+        }
+        private void findMatchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = true;
+            panel4.Visible = true;
+            panel1.Visible = false;
+            panel2.Visible = false;
+            teammatch = "select team_id as 'ID',team_name as'Team Name' from team";
+            sqlConnection = new MySqlConnection(connection);
+            mysqlcommand = new MySqlCommand(teammatch, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtteammatch);
+            comboBox3.DataSource = dtteammatch;
+            comboBox3.ValueMember = "ID";
+            comboBox3.DisplayMember = "Team Name";
+            comboBox3.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 400;
+            int y = 70;
+            label1 = new Label[10];
+            string[] label2 = label.Split(',');
+            for (int i = 0; i < 10; i++)
+            {
+                label1[i] = new Label();
+                label1[i].Text = label2[i];
+                label1[i].Location = new Point(x, y);
+                label1[i].Size = new Size(200, 30);
+                label1[i].Font = new Font("Arial", 20, FontStyle.Bold);
+                this.panel1.Controls.Add(label1[i]);
+                y += 60;
+            }
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            dtplayer.Clear();
+            comboBox2.Text = "";
+            player = "select p.player_name as 'name',p.player_id as 'IDplayer' from player p,team t where p.team_id=t.team_id and t.team_id=" + $"'{comboBox1.SelectedValue.ToString()}';";
+            mysqlcommand = new MySqlCommand(player, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtplayer);
+            comboBox2.DataSource = dtplayer;
+            comboBox2.ValueMember = "IDplayer";
+            comboBox2.DisplayMember = "name";
+            comboBox2.Text = "";
+        }
+
+        private void comboBox2_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            label3 = new Label[10];
+            label4 = new Label[10];
+            this.panel2.Controls.Clear();
+            dtplayerprofile2.Clear();
+            dtplayerprofile.Clear();
+            playerprofile = "select p.player_name as 'player name',t.team_name as 'team name',n.nation as 'nationality',p.team_number as 'pemain number' from player p ,team t,nationality n where t.team_id=p.team_id and p.nationality_id=n.nationality_id and p.player_id =" + $"'{comboBox2.SelectedValue.ToString()}';";
+            mysqlcommand = new MySqlCommand(playerprofile, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtplayerprofile);
+            playerprofile2 = "select COALESCE(sum(if(d.type='CY',1,0)),0) as 'Yellow Card',COALESCE(sum(if(d.type='CR',1,0)),0) as'Red Card',COALESCE(sum(if(d.type='GO',1,0)),0) as 'goal',COALESCE(sum(if(d.type='GW',1,0)),0) as 'Own Goal' ,COALESCE(sum(if(d.type='GP',1,0)),0) as 'Goal Penalty',COALESCE(sum(if(d.type='PM',1,0)),0) as 'pm'from dmatch d,player p where p.player_id = d.player_id and p.player_id=" + $"'{comboBox2.SelectedValue.ToString()}';";
+            mysqlcommand = new MySqlCommand(playerprofile2, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtplayerprofile2);
+            int x = 80;
+            int y = 70;
+            for (int i = 0; i < 4; i++)
+            {
+                label3[i] = new Label();
+                label3[i].Text = dtplayerprofile.Rows[0][i].ToString();
+                label3[i].Location = new Point(x, y);
+                label3[i].Size = new Size(200, 30);
+                label3[i].Font = new Font("Arial", 16, FontStyle.Bold);
+                this.panel2.Controls.Add(label3[i]);
+                y += 60;
+            }
+            int t = 80;
+            int p = 310;
+            for(int u = 0; u < 6; u++)
+            {
+                label4[u] = new Label();
+                label4[u].Text = dtplayerprofile2.Rows[0][u].ToString();
+                label4[u].Location = new Point(t, p);
+                label4[u].Size = new Size(200, 30);
+                label4[u].Font = new Font("Arial", 16, FontStyle.Bold);
+                this.panel2.Controls.Add(label4[u]);
+                p += 60;
+            }
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            dtvsmatch.Clear();
+            vsmatch = "select t1.team_id as 'teamid', m.match_id as'matchid',m.match_date as'match date',t1.team_name as'home team',t2.team_name as'away team' from `match`m,dmatch d,team t1,team t2 where m.team_home=t1.team_id and m.team_away=t2.team_id and m.match_id=d.match_id and (t1.team_id="+$"'{comboBox3.SelectedValue.ToString()}'or t2.team_id="+$"'{comboBox3.SelectedValue.ToString()}') group by m.match_id;";
+            sqlConnection = new MySqlConnection(connection);
+            mysqlcommand = new MySqlCommand(vsmatch, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtvsmatch);
+            comboBox4.DataSource = dtvsmatch;
+            comboBox4.ValueMember ="matchid";
+            comboBox4.DisplayMember = "matchid";
+
+        }
+
+        private void comboBox4_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            dtteamaway.Clear();
+            dtteamhome.Clear();
+            dtmatchdetail.Clear();
+            matchdetail = "select d.`minute` as'minute',t.team_name as'team name',p.player_name as 'playername',if(d.type='CY','Yellow Card',if(d.type='CR','Red Card',if(d.type='GO','Goal',if(d.type='GW','OwnGoal',if(d.type='Gp','Goal Penalty','Penalty Miss')))))from dmatch d,team t,player p where d.team_id=t.team_id and d.player_id=p.player_id and d.match_id=" + $"'{comboBox4.SelectedValue.ToString()}';";
+            sqlConnection = new MySqlConnection(connection);
+            mysqlcommand = new MySqlCommand(matchdetail, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtmatchdetail);
+            dataGridView1.DataSource = dtmatchdetail;
+            teamhome = "select t.team_name as'home team',p.player_name as 'home player',p.playing_pos as'position'from `match`m,team t,player p where p.team_id=t.team_id and t.team_id=m.team_home and m.match_id=" + $"'{comboBox4.SelectedValue.ToString()}';";
+            sqlConnection = new MySqlConnection(connection);
+            mysqlcommand = new MySqlCommand(teamhome, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtteamhome);
+            dataGridView2.DataSource = dtteamhome;
+            teamaway = "select t.team_name as'away team',p.player_name as 'away player',p.playing_pos as'position'from `match`m,team t,player p where p.team_id=t.team_id and t.team_id=m.team_away and m.match_id=" + $"'{comboBox4.SelectedValue.ToString()}';";
+            sqlConnection = new MySqlConnection(connection);
+            mysqlcommand = new MySqlCommand(teamaway, sqlConnection);
+            MySqlDataAdapter = new MySqlDataAdapter(mysqlcommand);
+            MySqlDataAdapter.Fill(dtteamaway);
+            dataGridView3.DataSource = dtteamaway;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
